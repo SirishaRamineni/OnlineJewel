b15f102f0fc1413ef6504b6de8a9cc3d2f107e9e
SirishaRamineni <131257669+SirishaRamineni@users.noreply.github.com>
2024-07-02 17:04:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,41 +4,39 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-import cors from 'cors'
-import categoryRoutes from "./routes/categoryRoutes.js"
-import productRoutes from "./routes/productRoutes.js"
-import path from 'path'
+import cors from 'cors';
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-//esmodule fix
+// esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const app=express()
+const app = express();
 
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
+// routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
-//middlewares
-app.use(express.json())
-app.use(cors())
-app.use(morgan('dev'))
+// static files
+app.use(express.static(path.join(__dirname, "./client/build")));
 
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
-//routes
-app.use("/api/v1/auth",authRoutes)
-app.use("/api/v1/category",categoryRoutes)
-app.use("/api/v1/product",productRoutes)
+const PORT = 8000;
 
-//static files
-
-app.use(express.static(path.join(__dirname,"./client/build")))
-
-app.use("*",function(req,res){
-    res.sendFile(path.join(__dirname,'./client/build/index.html'))
-})
-
-
-const PORT=8000
-
-app.listen(PORT,()=>console.log(`server running on port ${PORT}`.bgCyan.white))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`.bgCyan.white));
